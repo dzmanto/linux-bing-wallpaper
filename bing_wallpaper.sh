00fe6dabfaac5bfe8fa64e9f193c143a28b2e2b9
dzmanto <dzmanto@hotmail.com>
2016-11-04 21:55:17 +0100

    Permanently fixed the shebang line.
    Uncluttered various if-statements.

--- bing_wallpaper.sh
+++ bing_wallpaper.sh
@@ -1,47 +1,147 @@
 #!/bin/sh
-# Author: Marguerite Su <i@marguerite.su>
+# Author: Marguerite Su <i@marguerite.su>, dzmanto <dzmanto@hotmail.com>
 # License: GPL-3.0
 # Description: Download Bing Wallpaper of the Day and set it as your Linux Desktop.
 # https://github.com/marguerite/linux-bing-wallpaper
 
-function contains() {
-    local n=$#
-    local value=${!n}
-    for ((i=1;i < $#;i++)) {
-        if [ "${!i}" == "${value}" ]; then
+contains() {
+    local value=$(eval "echo \$$#")
+    count=1
+    for i in $*
+    do
+        if [ "$i" = "$value" -a $count -lt $# ]; then
             echo "y"
             return 0
         fi
-    }
+ 	count=$(expr $count + 1)
+    done
     echo "n"
     return 1
 }
 
-# cron  needs the DBUS_SESSION_BUS_ADDRESS environment variable set
-if [ -z "$DBUS_SESSION_BUS_ADDRESS" ] ; then
-  TMP=~/.dbus/session-bus
-  export $(grep -h DBUS_SESSION_BUS_ADDRESS= $TMP/$(ls -1t $TMP | head -n 1))
-fi
+detectDE() {
+    # see https://bugs.freedesktop.org/show_bug.cgi?id=34164
+    unset GREP_OPTIONS
 
-if [ "$#" == 0 ] ; then
+    uname -a | grep -i darwin 2>&1 >/dev/null && DE="mac";
+
+    if [ -n "${XDG_CURRENT_DESKTOP}" ]; then
+      case "${XDG_CURRENT_DESKTOP}" in
+         'GNOME')
+           DE="gnome"
+           ;;
+         'KDE')
+           DE="kde"
+           ;;
+         'LXDE')
+           DE="lxde"
+           ;;
+         'LXQt')
+           DE="lxqt"
+           ;;
+         'MATE')
+           DE="mate"
+           ;;
+         'XFCE')
+           DE="xfce"
+           ;;
+         'X-Cinnamon')
+           DE="cinnamon"
+           ;; 
+      esac
+    fi
+
+    if [ x"$DE" = x"" ]; then
+      # classic fallbacks
+      if [ x"$KDE_FULL_SESSION" = x"true" ]; then DE=kde;
+      elif [ x"$GNOME_DESKTOP_SESSION_ID" != x"" ]; then DE=gnome;
+      elif [ x"$MATE_DESKTOP_SESSION_ID" != x"" ]; then DE=mate;
+      elif `dbus-send --print-reply --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.GetNameOwner string:org.gnome.SessionManager > /dev/null 2>&1` ; then DE=gnome;
+      elif xprop -root _DT_SAVE_MODE 2> /dev/null | grep ' = \"xfce4\"$' >/dev/null 2>&1; then DE=xfce;
+      elif xprop -root 2> /dev/null | grep -i '^xfce_desktop_window' >/dev/null 2>&1; then DE=xfce
+      fi
+    fi
+
+    if [ x"$DE" = x"" ]; then
+      # fallback to checking $DESKTOP_SESSION
+      case "$DESKTOP_SESSION" in
+         'gnome')
+           DE="gnome"
+           ;;
+         'LXDE'|'Lubuntu')
+           DE="lxde"
+           ;;
+         'MATE')
+           DE="mate"
+           ;;
+         'xfce'|'xfce4'|'Xfce Session')
+           DE="xfce"
+           ;;
+      esac
+    fi
+
+    if [ x"$DE" = x"gnome" ]; then
+      # gnome-default-applications-properties is only available in GNOME 2.x
+      # but not in GNOME 3.x
+      which gnome-default-applications-properties > /dev/null 2>&1  || DE="gnome3"
+    fi
+    
+    echo $DE
+}
+
+if [ $# -eq 0 ]; then
   # The mkt parameter determines which Bing market you would like to
   # obtain your images from.
+
   mkt="zh-CN"
+  # Try and guess language
+  ML=$(echo $LANG | cut -f 1 -d .)
+  case $ML in
+	'en_US')
+	mkt="en-US"
+	;;
+	'zh_CN')
+	mkt="zh-CN"
+	;;
+	'ja_JP')
+	mkt="ja-JP"
+	;;
+	'en_AU')
+	mkt="en-AU"
+	;;
+	'en_UK')
+	mkt="en-UK"
+	;;
+	'de_CH')
+	mkt="de-DE"
+	;;
+	'de_DE')
+	mkt="de-DE"
+	;;
+	'en_NZ')
+	mkt="en-NZ"
+	;;
+	'en_CA')
+	mkt="en-CA"
+	;;
+  esac
   exitAfterRunning=false
 
-elif [ "$#" == 2 ] ; then
+elif [ $# -eq 2 ]; then
+  list="de-DE en-AU en-CA en-NZ en-UK en-US ja-JP zh-CN"
   # Valid values are:
-  declare -a list=("en-US" "zh-CN" "ja-JP" "en-AU" "en-UK" "de-DE" "en-NZ" "en-CA")
-
-  if [ $(contains "${list[@]}" $1) == "y" ]; then
-    mkt=$1
+  firstpar="$1"
+  #inhibit code injection
+  firstpar=$(echo "$firstpar" | sed s/[^a-zA-Z-]// )
+  if [ "$(contains $list $firstpar)" = "y" ]; then
+    mkt=$firstpar
   else
     echo "mkt must be one of the following:"
-    printf '%s\n' "${list[@]}"
+    printf '%s\n' "$list"
     exit 1
   fi
 
-  if [ "$2" = true ] ; then
+  if [ "$2" = true ]; then
     exitAfterRunning=true
   else
     exitAfterRunning=false
@@ -73,154 +173,96 @@ mkdir -p $saveDir
 
 # Set picture options
 # Valid options are: none,wallpaper,centered,scaled,stretched,zoom,spanned
-picOpts="zoom"
+picOpts="stretched"
 
 # The file extension for the Bing pic
 picExt=".jpg"
 
-detectDE()
-{
-    # see https://bugs.freedesktop.org/show_bug.cgi?id=34164
-    unset GREP_OPTIONS
-
-    if [ -n "${XDG_CURRENT_DESKTOP}" ]; then
-      case "${XDG_CURRENT_DESKTOP}" in
-         GNOME)
-           DE=gnome;
-           ;;
-         KDE)
-           DE=kde;
-           ;;
-         LXDE)
-           DE=lxde;
-           ;;
-         LXQt)
-           DE=lxqt;
-           ;;
-         MATE)
-           DE=mate;
-           ;;
-         XFCE)
-           DE=xfce
-           ;;
-         X-Cinnamon)
-           DE=cinnamon
-           ;; 
-      esac
-    fi
-
-    if [ x"$DE" = x"" ]; then
-      # classic fallbacks
-      if [ x"$KDE_FULL_SESSION" = x"true" ]; then DE=kde;
-      elif [ x"$GNOME_DESKTOP_SESSION_ID" != x"" ]; then DE=gnome;
-      elif [ x"$MATE_DESKTOP_SESSION_ID" != x"" ]; then DE=mate;
-      elif `dbus-send --print-reply --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.GetNameOwner string:org.gnome.SessionManager > /dev/null 2>&1` ; then DE=gnome;
-      elif xprop -root _DT_SAVE_MODE 2> /dev/null | grep ' = \"xfce4\"$' >/dev/null 2>&1; then DE=xfce;
-      elif xprop -root 2> /dev/null | grep -i '^xfce_desktop_window' >/dev/null 2>&1; then DE=xfce
-      fi
-    fi
-
-    if [ x"$DE" = x"" ]; then
-      # fallback to checking $DESKTOP_SESSION
-      case "$DESKTOP_SESSION" in
-         gnome)
-           DE=gnome;
-           ;;
-         LXDE|Lubuntu)
-           DE=lxde;
-           ;;
-         MATE)
-           DE=mate;
-           ;;
-         xfce|xfce4|'Xfce Session')
-           DE=xfce;
-           ;;
-      esac
-    fi
-
-    if [ x"$DE" = x"gnome" ]; then
-      # gnome-default-applications-properties is only available in GNOME 2.x
-      # but not in GNOME 3.x
-      which gnome-default-applications-properties > /dev/null 2>&1  || DE="gnome3"
-    fi
-}
-
 # Download the highest resolution
 while true; do
 
-    TOMORROW=$(date --date="tomorrow" +%Y-%m-%d)
-    TOMORROW=$(date --date="$TOMORROW 00:10:00" +%s)
-
+    picName=""
+    picURL=""
+    
     for picRes in _1920x1200 _1366x768 _1280x720 _1024x768; do
 
-    # Extract the relative URL of the Bing pic of the day from
-    # the XML data retrieved from xmlURL, form the fully qualified
-    # URL for the pic of the day, and store it in $picURL
-    picURL=$bing$(echo $(curl -s $xmlURL) | grep -oP "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)$picRes$picExt
-
-    # $picName contains the filename of the Bing pic of the day
-    picName=${picURL##*/}
-
-    # Download the Bing pic of the day
-    curl -s -o $saveDir$picName -L $picURL
-
-    # Test if download was successful.
-    downloadResult=$?
-    if [[ $downloadResult -ge 1 ]]; then
-        rm -rf $saveDir$picName && continue
-    fi
-
-    # Test if it's a pic
-    file --mime-type -b $saveDir$picName | grep "^image/" > /dev/null && break
-
-    rm -rf $saveDir$picName
+	    # Extract the relative URL of the Bing pic of the day from
+	    # the XML data retrieved from xmlURL, form the fully qualified
+	    # URL for the pic of the day, and store it in $picURL
+	    picURL=$bing$(echo $(curl -s $xmlURL) | egrep -o "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)$picRes$picExt
+	    
+	    # $picName contains the filename of the Bing pic of the day
+	    # picName=${picURL##*/}
+	    picName="bing_wallpaper_$$$picExt"
+	
+	    # Download the Bing pic of the day
+	    curl -s -o $saveDir$picName -L "$picURL"
+	
+	    # Test if download was successful.
+	    downloadResult=$?
+	    if [ $downloadResult -ge 1 ]; then
+		rm -f $saveDir$picName && continue
+	    fi
+	
+	    if [ -x "/usr/bin/convert" ]; then
+	    	title=$(echo $(curl -s $xmlURL) | egrep -o "<copyright>(.*)</copyright>" | cut -d ">" -f 2 | cut -d "<" -f 1 )
+	    	convert -background "#00000080" -fill white -gravity center -size 1024 -font "Droid Sans" -pointsize 22 caption:"${title}" $saveDir$picName +swap -gravity south -composite $saveDir$picName
+    	    fi
+	    # Test if it's a pic
+	    file --mime-type -b $saveDir$picName | grep "^image/" > /dev/null && break
+	
+	    rm -f $saveDir$picName
     done
-    detectDE
 
-    if [[ downloadResult -ge 1 ]]; then
-          echo "Couldn't download any picture."
-          exit 0;
+    if [ $downloadResult -ge 1 ]; then
+          echo "Failed to download any picture."
+	  echo "Try again in 60 seconds."
+          sleep 60
+          continue
     fi
 
-    if [[ $DE = "cinnamon" ]]; then
+    DE=$(detectDE)
+
+    if [ "$DE" = "cinnamon" ]; then
           # Set the Cinnamon wallpaper
           DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.cinnamon.desktop.background picture-uri '"file://'$saveDir$picName'"'
 
           # Set the Cinnamon wallpaper picture options
           DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.cinnamon.desktop.background picture-options $picOpts
-    fi
-
-    if [[ $DE = "gnome" ]]; then
-      # Set the GNOME 2 wallpaper
-      gconftool-2 -s -t string /desktop/gnome/background/picture_filename "$saveDir$picName"
-
-      # Set the GNOME 2 wallpaper picture options
-      gconftool-2 -s -t string /desktop/gnome/background/picture_options "$picOpts"
-    fi
-
-    if [[ $DE = "gnome3" ]]; then
+    elif [ "$DE" = "gnome" ]; then
+	# Set the GNOME 2 wallpaper
+	gconftool-2 -s -t string /desktop/gnome/background/picture_filename "$saveDir$picName"
+	
+	# Set the GNOME 2 wallpaper picture options
+	gconftool-2 -s -t string /desktop/gnome/background/picture_options "$picOpts"
+    elif [ "$DE" = "gnome3" ]; then
       # Set the GNOME3 wallpaper
       DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background picture-uri '"file://'$saveDir$picName'"'
 
       # Set the GNOME 3 wallpaper picture options
       DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background picture-options $picOpts
-    fi
-
-    if [[ $DE = "gnome3" ]]; then
-    gsettings set org.gnome.desktop.background picture-uri '"file://'$saveDir$picName'"'
-    fi
-
-    if [[ $DE = "kde" ]]; then
+      gsettings set org.gnome.desktop.background picture-uri '"file://'$saveDir$picName'"'
+    elif [ "$DE" = "kde" ]; then
       test -e /usr/bin/xdotool || sudo zypper --no-refresh install xdotool
       test -e /usr/bin/gettext || sudo zypper --no-refresh install gettext-runtime
       ./kde4_set_wallpaper.sh $saveDir$picName
-    fi
-
-    if [[ $DE = "lxqt" ]] ; then
+    elif [ "$DE" = "lxqt" ] ; then
       pcmanfm-qt -w $saveDir$picName
-    fi
-
-    if [[ $DE = "xfce" ]]; then
+    elif [ "$DE" = "mac" ]; then
+	fn=$saveDir$picName
+	osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'"$fn"'"'
+	osaResult=$?
+	sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$fn'" && killall Dock 
+	sqliteResult=$?
+	if [ $osaResult -ge 1 -a $sqliteResult -ge 1 ]; then
+		echo "Failed to refresh desktop image."
+	        echo "Try again in 60 seconds."
+		sleep 60
+		continue
+	fi
+    elif [ "$DE" = "mate" ]; then
+      dconf write /org/mate/desktop/background/picture-filename '"'$saveDir$picName'"'
+    elif [ "$DE" = "xfce" ]; then
       ./xfce4_set_wallpaper.sh $saveDir$picName
     fi
 
@@ -229,7 +271,12 @@ while true; do
       exit 0
     fi
 
-    NOW=$(date +%s)
-    SLEEP=`echo $TOMORROW-$NOW|bc`
-    sleep $SLEEP
+    # sleep for half a day
+    DIFF_TIME=0
+    LAST_RUN=$(date +%s)
+    while [ $DIFF_TIME -lt 43199 ]; do
+	NOW=$(date +%s)
+	DIFF_TIME=$(expr $NOW - $LAST_RUN)
+	sleep 60
+    done
 done
